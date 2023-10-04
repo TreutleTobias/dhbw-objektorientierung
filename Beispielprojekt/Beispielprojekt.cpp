071aeaa3db383f50661157a68c9b8b40962fa5f8
Flecky Streifen <flecky-hase@hotmail.de>
2023-10-04 13:22:06 +0200

    Viereck

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -1,4 +1,4 @@
-#include <Gosu/Gosu.hpp>
+/*#include <Gosu/Gosu.hpp>
 #include <Gosu/AutoLink.hpp>
 
 class GameWindow : public Gosu::Window
@@ -17,12 +17,7 @@ public:
 	void draw() override
 	{
 
-		Gosu::Graphics::draw_triangle(
-			x, y, Gosu::Color::RED,
-			200, 100, Gosu::Color::GREEN,
-			200, 400, Gosu::Color::BLUE,
-			0.0
-		);
+	drawQuad(0, 0, 0xff000000);
 	}
 	double x = 0;
 	double y = 0;
@@ -41,4 +36,27 @@ int main()
 {
 	GameWindow window;
 	window.show();
+
+}
+*/
+#include <Gosu/Gosu.hpp>
+#include <Gosu/AutoLink.hpp>
+
+class MyWindow : public Gosu::Window {
+public:
+	MyWindow() : Gosu::Window(800, 600) {
+	}
+
+	void draw() {
+		// Löschen Sie den Bildschirm
+		graphics().draw_quad(100.0, 400.0,Gosu::Color::GREEN, 200.0, 500.0, Gosu::Color::GREEN, 300.0, 800.0, Gosu::Color::GREEN,400.0, 600.0, Gosu::Color::GREEN,0);
+
+		
+	}
+};
+
+int main() {
+	MyWindow window;
+	window.show();
+	return 0;
 }
